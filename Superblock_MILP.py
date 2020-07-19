aa9c30f00571bdb10352dad3619ebf6bd46d0ec3
Theresa Wettig <theresa.wettig@tum.de>
2020-07-19 10:30:15 +0200

    superblock programm added + parts of the preprocessing

---
+++ Superblock_MILP.py
@@ -0,0 +1,177 @@
+import gurobipy as gp
+from gurobipy import GRB
+from pandas import *
+from preprocessing.GenerationSubsets import *
+
+# parameters (will later be initialized in separated script)
+I_center = [100, 100, 100]  # number of specific centers per center type
+Center_Total = 300
+I = sum(i for i in I_center)  # number of total placable centers (general centers)
+I_c = create_specific_centers(I_center)  # specific centers per center type
+C = {'museum', 'doctor', 'police'}  # center types
+SB = 9  # number of superblocks
+G_count = 2  # number of gates per superblock
+G = G_count * SB  # number of total gates of city (general gates)
+G_sb = create_gsb(G_count, SB)  # specific gates per superblock
+demand_c = [5, 20, 15]  # demand per center type
+capacity_c = [100, 100, 100]  # capacity per center type
+beta = 0.01
+area_c = [100, 450, 716]  # area needed per center type
+maxArea = 1000  # maxArea available for center placement in each superblock
+# distance matrix
+filePath = 'preprocessing/Distance_Matrix_Layout_1.xlsx'
+distances = pandas.read_excel(filePath, header=None)
+maxDist_c = [3000, 3000, 3000]  # maximum Distances to center types
+M = 500000  # big M
+
+# SuperblockImplementation
+m = gp.Model('Superblock')
+
+# decision variables
+visitors = {}
+for g1 in range(G):
+    for g2 in range(G):
+        for i in range(I):
+            visitors[g1, g2, i] = m.addVar(vtype=GRB.CONTINUOUS, name='visitors')
+
+selfVisitors = {}
+for sb in range(SB):
+    for i in range(I):
+        selfVisitors[sb, i] = m.addVar(vtype=GRB.CONTINUOUS, name='selfVisitors')
+
+visFreq = {}
+for sb in range(SB):
+    for i in range(I):
+        visFreq[sb, i] = m.addVar(vtype=GRB.CONTINUOUS, name='visFreq')
+
+placementKey = {}
+for sb2 in range(SB):
+    for i in range(I):
+        placementKey[sb2, i] = m.addVar(vtype=GRB.BINARY, name='placementKey' + str(sb2) + ' ' + str(i))
+
+z = {}
+for g1 in range(G):
+    for g2 in range(G):
+        for i in range(I):
+            z[g1, g2, i] = m.addVar(vtype=GRB.BINARY, name='z')
+
+m.update()
+# objective function
+# Minimize sum of traveled distances
+m.setObjective(sum(sum(sum(visitors[g1, g2, i] * distances.iloc[g1, g2]
+                           for g2 in range(G)) for g1 in range(G)) for i in range(I)), GRB.MINIMIZE)
+
+# constraints
+# constraint 1
+# Sum of all Visitors and selfVisitors from a specific gate in a superblock
+# to all general gates, for a specific center
+# equal the visitor frequency for the specific center for all superblocks and all specific centers
+for sb in range(SB):
+    gates_of_sb = G_sb[sb]  # e.g. sb = 1 G_sb = [0, 1]; sb = 2 G_sb = [2, 3]
+    for i in range(I):
+        m.addConstr((sum(sum(visitors[g1, g2, i] for g2 in range(G))
+                         for g1 in gates_of_sb) + selfVisitors[sb, i] == visFreq[sb, i]), "1")
+
+# constraint 1a
+# Greater/equal zero constraint for visitors for all general gates and all specific centers
+for g1 in range(G):
+    for g2 in range(G):
+        for i in range(I):
+            m.addConstr((visitors[g1, g2, i] >= 0), "1a")
+
+# constraint 1b
+# Greater/equal zero constraint for selfVisitors for all superblocks and all specific centers
+for sb in range(SB):
+    for i in range(I):
+        m.addConstr((selfVisitors[sb, i] >= 0), "1b")
+
+# constraint 1c
+# visitors from g1 of sb to g2 of sb must be equal to zero for all superblocks,
+# for all block specific gates and all specific centers
+for sb in range(SB):
+    gates_of_sb = G_sb[sb]
+    for g1 in gates_of_sb:
+        for g2 in gates_of_sb:
+            for i in range(I):
+                m.addConstr((visitors[g1, g2, i] == 0), "1c")
+
+# constraint 2a
+# Sum of visitors frequency overall superblocks is greater/equal to minimal required utilization for all center types
+# and for all specific centers
+for c in range(len(C)):
+    for i_c in (I_c[c]):
+        m.addConstr(sum(visFreq[sb, i_c] for sb in range(SB)) >= capacity_c[c] * beta * sum(
+            placementKey[sb, i_c] for sb in range(SB)), "2a")
+
+# constraint 2b
+# Sum of visitors frequency overall superblocks is less/equal the utilization for all center types
+# and for all specific centers
+for c in range(len(C)):
+    for i_c in (I_c[c]):
+        m.addConstr(sum(visFreq[sb, i_c] for sb in range(SB)) <= capacity_c[c], "2b")
+
+# constraint 3
+# Sum of visitor frequency overall specific centers is greater/equal center type demand
+# for all center types and all superblocks
+for c in range(len(C)):
+    for sb in range(SB):
+        m.addConstr((sum(visFreq[sb, i_c] for i_c in (I_c[c])) >= demand_c[c]), "3")
+
+# constraint 4a
+# Big M constraint
+# if center is placed visitors must be less than threshold M for all superblocks, for all general centers,
+# for all  general gates and for block specific gates
+for sb in range(SB):
+    # print(sb)
+    gates_of_sb = G_sb[sb]
+    for i in range(I):
+        # print(i)
+        for g1 in range(G):
+            # print(g1)
+            for g2 in gates_of_sb:
+                # print (g2)
+                m.addConstr((visitors[g1, g2, i]
+                             <= placementKey[sb, i] * M), "4a")
+
+# constraint 4b
+# Big M constraint
+# If center is placed visitors to SelfVisitors to center must be less than threshold M for all superblocks
+# and for all general centers
+for sb in range(SB):
+    for i in range(I):
+        m.addConstr((selfVisitors[sb, i]
+                     <= placementKey[sb, i] * M), "4b")
+
+# constraint 4c
+# Sum of placementKey overall superblocks must be less/equal 1 for all general centers
+# (each center can only be placed maximal once)
+for i in range(Center_Total):
+    m.addConstr(sum(placementKey[sb, i] for sb in range(SB)) <= 1, "4c")
+
+# constraint 5
+# Distances that are traveled from g1 to g2 for a specific center must be less/equal the maxDistance
+# per center type for all center types, specific centers and general gates
+for c in range(len(C)):
+    for i_c in I_c[c]:
+        for g1 in range(G):
+            for g2 in range(G):
+                m.addConstr((z[g1, g2, i_c] * distances.iloc[g1, g2] <= maxDist_c[c]), "5")
+
+# constraint 5a
+# Big M constraint that sets z[g1, g2, i] == 1 if there are visitors from g1 to g2 for i
+for c in range(len(C)):
+    for i in I_c[c]:
+        for g1 in range(G):
+            for g2 in range(G):
+                m.addConstr((visitors[g1, g2, i] <= z[g1, g2, i] * M), "5a")
+
+# constraint 6
+# The areas of all placed specific centers must be less/equal the maximum available area
+# in a superblock for all superblocks
+for sb in range(SB):
+    m.addConstr((sum(sum(area_c[c] * placementKey[sb, i] for i in (I_c[c])) for c in
+                     range(len(C))) <= maxArea), "6a")
+
+# execution
+m.setParam('TimeLimit', 30)
+m.optimize()
---
+++ preprocessing/Distancematrix.py
@@ -0,0 +1,52 @@
+import numpy as np
+import xlsxwriter
+
+
+# config 1 describes placement of gates in a horizontal line in the middle of each block
+def generate_distance_matrix_config_1( num_gb_in_row, num_gb_per_col, gates_p_sb, street_width, block_width):
+    workbook = xlsxwriter.Workbook('Distance_Matrix_Layout_1.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    num_sb_per_gb = 9
+
+    # depending on the particular layout : get gates in one row / gates in one column
+    # here: gates are placed on a horizontal axis in the middle of each superblock
+    gates_total = num_gb_per_col * num_gb_in_row * num_sb_per_gb * gates_p_sb
+    gates_in_one_row = num_gb_in_row * (num_sb_per_gb ** 0.5) * gates_p_sb
+    gates_in_one_col = num_gb_per_col * (num_sb_per_gb ** 0.5)
+    gate_distance_matrix = np.zeros((gates_total, gates_total))
+    for a in range(gates_total):
+        for b in range(gates_total):
+
+            # get horizontal distance
+            pos_x_axis_start = a % gates_in_one_row if a % gates_in_one_row < b % gates_in_one_row else b % gates_in_one_row
+            pos_x_axis_end =  b % gates_in_one_row if a % gates_in_one_row < b % gates_in_one_row else a % gates_in_one_row
+
+            # get num of superblock - distances (horizontally) by getting number of blocks between gates
+            pos_x_axis_start_blocks = pos_x_axis_start - 1 if pos_x_axis_start % 2 == 0 and pos_x_axis_start != pos_x_axis_end else pos_x_axis_start
+            num_block_dists = ( pos_x_axis_end - pos_x_axis_start_blocks) // 2
+
+            # do the same for streets
+            pos_x_axis_start_streets = pos_x_axis_start - 1 if pos_x_axis_start % 2 == 1  and pos_x_axis_start != pos_x_axis_end else pos_x_axis_start
+            num_streets_dists = (pos_x_axis_end - pos_x_axis_start_streets) // 2
+            total_dist_x = num_block_dists * block_width + num_streets_dists * street_width
+            print(total_dist_x)
+
+            # get vertical distance (here it's easier)
+            pos_y_axis_start = a // gates_in_one_row if  a // gates_in_one_row < b // gates_in_one_row else b // gates_in_one_row
+            pos_y_axis_end = b // gates_in_one_row if a // gates_in_one_row < b // gates_in_one_row else a // gates_in_one_row
+            num_dists = (pos_y_axis_end - pos_y_axis_start)
+            total_dist_y = num_dists * (block_width + street_width)
+
+            # for this configuration, we have to take into account that if two blocks are placed horizontally,
+            # we also have to go down / up first and can not just get the linear distance
+            bypass = block_width if pos_y_axis_end == pos_y_axis_start and pos_x_axis_start != pos_x_axis_end else 0
+
+            gate_distance_matrix[a, b] = total_dist_x + total_dist_y + bypass
+            worksheet.write(a, b,  total_dist_x + total_dist_y + bypass)
+    workbook.close()
+    return gate_distance_matrix
+
+result = generate_distance_matrix_config_1(1, 1, 2, 20, 1000)
+
+print(result)
